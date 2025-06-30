12a0ed48220921e8211f0852346586d7a92da86e
Deusagnis <deusagnis@gmail.com>
2025-06-30 20:59:42 +0300

    Update version.

--- selenium_captcha_processing/__init__.py
+++ selenium_captcha_processing/__init__.py
@@ -21,7 +21,7 @@ from .detectors.interfaces.detector import DetectCaptchaI
 from .solvers.interfaces.solver import SolveCaptchaI
 from .utils.interfaces.speech_recogniser import SpeechRecogniserI
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __all__ = [
     'BypassCaptcha',
